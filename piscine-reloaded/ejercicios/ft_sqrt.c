f8dd7e0e652a5f6ac2a8310e9ec94dec7f3f5480
soyjuandelgado <soy.juan.delgado@gmail.com>
2025-10-01 20:45:18 +0200

    Crear ft_sqrt.c demo

---
+++ piscine-reloaded/ejercicios/ft_sqrt.c
@@ -0,0 +1,15 @@
+int		ft_sqrt_recur(int nb, int i)
+{
+	if (nb > 2147395600)
+		return (0);
+	if (i * i == nb)
+		return (i);
+	if (i * i < nb)
+		return (ft_sqrt_recur(nb, i + 1));
+	return (0);
+}
+
+int		ft_sqrt(int nb)
+{
+	return (ft_sqrt_recur(nb, 0));
+}
